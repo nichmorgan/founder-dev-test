5a781f455f93d313262888cac8adbfc40a01287e
nich.morgan <morgannicholson1371@gmail.com>
2023-09-14 19:01:11 -0300

    cors fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,10 +2,19 @@ from fastapi import FastAPI
 
 from app.core.config import settings
 from app.api import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_STR}/openapi.json",
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router)
